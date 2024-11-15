c18f2f31f7df0062d5d6c6874b08e1ebf4bf37e1
GoMyeongho <sdx02013@ajou.ac.kr>
2024-11-15 15:47:42 +0900

    24.11.15

--- js/jsonPractice.js
+++ js/jsonPractice.js
@@ -34,10 +34,23 @@ const jsonData = `
 ]
 `;
 
-// JSON 문자열을 JavaScript 객체로 변환 (역직렬화)
+//역직렬화를 해야 함.
 const jsonObject = JSON.parse(jsonData);
-console.log("객체로 변환된 데이터:", jsonObject);
 
-// JavaScript 객체를 JSON 문자열로 변환 (직렬화)
-const jsonString = JSON.stringify(jsonObject, null, 4);
-console.log("다시 JSON으로 변환된 데이터:", jsonString);
+const container = document.querySelector('#container');
+
+const htmlContent = jsonObject.map((person) =>
+  `<div class="person">
+    <h2>이름 : ${person.name}</h2>
+    <p>나이 : ${person.age}</p>
+    <p>학생 여부 : ${person.isStudent ? "예" : "아니오"}</p>
+    <p>과목 : ${person.courses.join(", ")}</p>
+    <p>주소 : ${person.address.city}, 우편번호 : ${person.address.zipcode}</p>
+  </div>`
+).join("");
+container.innerHTML = htmlContent;
+
+
+
+
+
